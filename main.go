78a7c2926097ca30a0fe565358cca21dbe3e4630
Herman Banken <hermanbanken@gmail.com>
2018-11-28 16:51:49 +0100

    Make it reusable

--- main.go
+++ main.go
@@ -43,8 +43,8 @@ func main() {
 	http.HandleFunc("/_ah/health", healthCheckHandler)
 
 	// Oauth2 client routes
-	http.HandleFunc("/login", handleHueLogin)
-	http.HandleFunc("/hue_callback_url", handleHueCallback)
+	http.HandleFunc("/login", HandleHueLogin)
+	http.HandleFunc("/hue_callback_url", HandleHueCallback(redirectToRootWithToken))
 
 	// Debugger application
 	http.HandleFunc("/", handleRoot)
@@ -62,7 +62,7 @@ func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
 	if strings.HasPrefix(r.URL.Path, "/api/") || strings.HasPrefix(r.URL.Path, "/v2/") || strings.HasPrefix(r.URL.Path, "/bridge/") || strings.HasPrefix(r.URL.Path, "/connectionstatus") {
-		handleRequestAndRedirect(w, r)
+		HandleRequestAndRedirect(w, r)
 		return
 	}
 	if r.URL.Path != "/" {
@@ -78,23 +78,31 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, htmlIndex)
 }
 
-func handleHueLogin(w http.ResponseWriter, r *http.Request) {
+// HandleHueLogin redirects to Hue Login
+func HandleHueLogin(w http.ResponseWriter, r *http.Request) {
 	url := hueOauthConfig.AuthCodeURL(oauthStateString)
 	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 }
 
-func handleHueCallback(w http.ResponseWriter, r *http.Request) {
-	content, err := getToken(r.FormValue("state"), r.FormValue("code"))
-	if err != nil {
-		fmt.Println(err.Error())
-		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-		return
-	}
-
-	http.Redirect(w, r, fmt.Sprintf("clip.html?accessToken=%s", content.AccessToken), http.StatusTemporaryRedirect)
+func redirectToRootWithToken(token *oauth2.Token, w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, fmt.Sprintf("clip.html?accessToken=%s", token.AccessToken), http.StatusTemporaryRedirect)
 }
 
-func getToken(state string, code string) (*oauth2.Token, error) {
+// HandleHueCallback redirects to
+func HandleHueCallback(handler func(token *oauth2.Token, w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		content, err := GetToken(r.FormValue("state"), r.FormValue("code"))
+		if err != nil {
+			fmt.Println(err.Error())
+			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+			return
+		}
+		handler(content, w, r)
+	}
+}
+
+// GetToken retrieves an OAuth token from the API
+func GetToken(state string, code string) (*oauth2.Token, error) {
 	if state != oauthStateString {
 		return nil, fmt.Errorf("invalid oauth state")
 	}
@@ -105,8 +113,8 @@ func getToken(state string, code string) (*oauth2.Token, error) {
 	return token, nil
 }
 
-// Serve a reverse proxy for a given url
-func serveReverseProxy(target string, res http.ResponseWriter, req *http.Request) {
+// ServeReverseProxy acts as a reverse proxy for a given url
+func ServeReverseProxy(target string, res http.ResponseWriter, req *http.Request) {
 	// parse the url
 	url, _ := url.Parse(target)
 	// create the reverse proxy
@@ -120,10 +128,10 @@ func serveReverseProxy(target string, res http.ResponseWriter, req *http.Request
 	proxy.ServeHTTP(res, req)
 }
 
-// Given a request send it to the appropriate url
-func handleRequestAndRedirect(res http.ResponseWriter, req *http.Request) {
+// HandleRequestAndRedirect Given a request send it to the appropriate url
+func HandleRequestAndRedirect(res http.ResponseWriter, req *http.Request) {
 	req.URL.Host = "api.meethue.com"
 	req.URL.Scheme = "https"
 	log.Printf("Proxy %s %s", req.Method, req.URL.String())
-	serveReverseProxy(req.URL.String(), res, req)
+	ServeReverseProxy(req.URL.String(), res, req)
 }
